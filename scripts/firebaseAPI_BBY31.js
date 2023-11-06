64fcc5b30c13adc4d32acfd907bb8893ef92915a
Victor Yang <vyang11@my.bcit.ca>
2023-11-05 19:50:54 -0800

    added login and signup pages, possibly redundant, added js for authentication

--- scripts/firebaseAPI_BBY31.js
+++ scripts/firebaseAPI_BBY31.js
@@ -1,14 +1,14 @@
 //----------------------------------------
 //  Your web app's Firebase configuration
 //----------------------------------------
-// var firebaseConfig = {
-//     apiKey: ,
-//     authDomain: ,
-//     projectId: ,
-//     storageBucket: ,
-//     messagingSenderId: ,
-//     appId:
-// };
+var firebaseConfig = {
+  apiKey: "AIzaSyChJ-g3KySOVzk6SJdwm9DsKGbgWFR72PE",
+  authDomain: "pricepalatebby31.firebaseapp.com",
+  projectId: "pricepalatebby31",
+  storageBucket: "pricepalatebby31.appspot.com",
+  messagingSenderId: "225587008385",
+  appId: "1:225587008385:web:5596d4b5b5ab3ab6200e80",
+};
 
 //--------------------------------------------
 // initialize the Firebase app
