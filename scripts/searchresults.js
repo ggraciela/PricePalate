e80be8418cd2817c2ecb5d3100b37cf2065daab7
gggraciela <ariwijayagraciela@gmail.com>
2023-11-30 21:33:33 -0800

    if add button was already clicked before or item is already added to the shopping list, meaning item is in current list, the button stays green and filled.

--- scripts/searchresults.js
+++ scripts/searchresults.js
@@ -1,11 +1,11 @@
 function displayCardsDynamically(collection) {
   allmarket();
   var cardTemplate = document.getElementById("searchresulttemplate");
-  
+
   // to match search result with the keyword
   let params = new URL(window.location.href); // Get the URL from the search bar
   let keyword = params.searchParams.get("value");
- 
+
   // to get value of checkboxes
   var walmartbox = (localStorage.getItem("walmartstat") === 'true');
   var costcobox = (localStorage.getItem("costcostat") === 'true');
@@ -22,7 +22,7 @@ function displayCardsDynamically(collection) {
     .where('tnt', "==", tntbox)
     .get()   //the collection 
     .then(allResults => {
-      if(allResults.size < 1){
+      if (allResults.size < 1) {
         document.getElementById('allresults').innerHTML = "<br><br><br><br><br><br><br><br><br>No results";
         document.getElementById('allresults').style.textAlign = "center";
         document.getElementById('allresults').style.margin = "auto";
@@ -43,17 +43,17 @@ function displayCardsDynamically(collection) {
           newcard.querySelector('.price').innerHTML = "$" + price;
           newcard.querySelector('.item').innerHTML = itemname;
           newcard.querySelector('.detail').innerHTML = detail;
-          newcard.getElementById("productimage").src = imgurl; 
+          newcard.getElementById("productimage").src = imgurl;
           newcard.getElementById("storelogo").src = storelogo;
-          
-          newcard.querySelector('button').id = 'add-' + itemid;  
 
-          // checkifalreadyclicked();
-          newcard.querySelector('button').onclick = () => additemtolist(itemid); 
+          newcard.querySelector('button').id = 'add-' + itemid;
+
+          checkifalreadyclicked(itemid);
+          newcard.querySelector('button').onclick = () => additemtolist(itemid);
 
           // adding the new card
           document.getElementById('allresults').appendChild(newcard);
-          
+
         })
       }
     })
@@ -70,8 +70,8 @@ function additemtolist(itemid) {
     if (user) {
       const currentUser = db.collection("users").doc(user.uid);
       currentUser.update({
-          currentList: firebase.firestore.FieldValue.arrayUnion(itemid),
-        })
+        currentList: firebase.firestore.FieldValue.arrayUnion(itemid),
+      })
         // Handle the front-end update to change the icon, providing visual feedback to the user that it has been clicked.
         .then(function () {
           console.log("product has been added into shoppinglist" + itemid);
@@ -82,8 +82,8 @@ function additemtolist(itemid) {
 
           // document.getElementById("add-" + itemid ).innerHTML = 'add_box'; 
           // document.getElementById("add-" + itemid).children[0].innerHTML = 'add_box'; 
-          document.getElementById("add-" + itemid).children[0].style = "font-variation-settings: 'FILL' 1, 'wght' 700, 'GRAD' 0, 'opsz' 48;"; 
-          document.getElementById("add-" + itemid).children[0].style.color = "#39A36A"; 
+          document.getElementById("add-" + itemid).children[0].style = "font-variation-settings: 'FILL' 1, 'wght' 700, 'GRAD' 0, 'opsz' 48;";
+          document.getElementById("add-" + itemid).children[0].style.color = "#39A36A";
 
           // document.getElementById("add-" + itemid).style. = "#39A36A";
           // document.getElementById("plusbtn").classList.add(""); 
@@ -94,14 +94,14 @@ function additemtolist(itemid) {
 
 
         });
-    } 
+    }
   });
 }
 
 
 
 // document.getElementById("plusbtn").target.classList.add("filled");
-          
+
 // function changebutton() {
 //   let allBtns = document.querySelectorAll("plusbtn")
 
@@ -109,15 +109,15 @@ function additemtolist(itemid) {
 // allBtns.forEach(function(elem){
 
 //   elem.addEventListener("click", function(e){
-  
+
 //     // On click, remove the MyClass on ALL buttons
 //     allBtns.forEach(function(el){
 //       el.classList.remove("filled");
 //     });
-    
+
 //     // Add the class on clicked one
 //     e.target.classList.add("filled");
-    
+
 //     // Now pass the data-href to your iframe
 //     // let theHREFtoOpen = e.target.getAttribute("data-href")
 //     // console.log(theHREFtoOpen)
@@ -128,12 +128,22 @@ function additemtolist(itemid) {
 
 
 
-function checkifalreadyclicked(){
-  db.collection("users").doc(user.uid).data().currentList.forEach((itemId) => {
-    if(itemId == itemid) {
-      document.getElementById("add-" + itemid).children[0].style = "font-variation-settings: 'FILL' 1, 'wght' 700, 'GRAD' 0, 'opsz' 48;"; 
-      document.getElementById("add-" + itemid).children[0].style.color = "#39A36A"; 
-    } 
+function checkifalreadyclicked(itemid) {
+  firebase.auth().onAuthStateChanged((user) => {
+    // Check if a user is signed in:
+    if (user) {
+      const currentUser = db.collection("users").doc(user.uid);
+
+      currentUser.get().then((userDoc) => {
+        const currentList = userDoc.data().currentList;
+
+        currentList.forEach((itemId) => {
+          if (itemId == itemid) {
+            document.getElementById("add-" + itemid).children[0].style = "font-variation-settings: 'FILL' 1, 'wght' 700, 'GRAD' 0, 'opsz' 48;";
+            document.getElementById("add-" + itemid).children[0].style.color = "#39A36A";
+          } 
+        })
+      })
+    }
   })
 }
-
