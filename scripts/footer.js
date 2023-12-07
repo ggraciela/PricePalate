bdb71a93290092cffc3f324d344a776eac917e15
gggraciela <ariwijayagraciela@gmail.com>
2023-12-07 10:12:15 -0800

    fixed search page button

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,9 +1,15 @@
 function searchwithlastkeyword(){
   var keyword = (localStorage.getItem("keywordsearch"));
 
+  document.getElementById("searchpage").style.color = "#39A36A";
+
   if (keyword != null) {
     window.location.href = "searchResults.html?value=" + keyword;
   } else {
     window.location.href = "searchResults.html"
+  }
+
+  if (window.location.href.includes("searchResults.html")) {
+    document.getElementById("searchpage").style.color = "#39A36A";
   }
 }
